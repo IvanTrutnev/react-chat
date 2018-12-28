ffc54a1321062051eb0dbe8a243c2eb0a2a8612e
IvanTrutnev <trutnev_1996@mail.ru>
2018-12-28 23:02:29 +0300

    use material ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,109 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import TextField from '@material-ui/core/TextField';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-         <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-        <NotificationsIcon />
-      </div>
-    );
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
   }
+});
+
+function PermanentDrawerLeft(props) {
+  const { classes } = props;
+  const chats = [
+    {name: 'Name', short_name: 'Short name', time: '6 hours ago'},
+    {name: 'Name2', short_name: 'Short name2', time: '6 hours ago'},
+    {name: 'Name3', short_name: 'Short name3', time: '6 hours ago'},
+    {name: 'Name4', short_name: 'Short name4', time: '6 hours ago'},
+    {name: 'Name5', short_name: 'Short name5', time: '6 hours ago'},
+    {name: 'Name6', short_name: 'Short name6', time: '6 hours ago'}
+  ]
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            React Talks
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <TextField
+          id="standard-with-placeholder"
+          label="With placeholder"
+          placeholder="Placeholder"
+          className={classes.textField}
+          margin="normal"
+        />
+        <Divider />
+        <List>
+          {chats.map((item, index) => (
+            <ListItem key={item.name}>
+              <ListItemIcon>{item.name}</ListItemIcon>
+            </ListItem>
+            // <ListItem button key={item.short_name}>
+            //   <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            //   <ListItemText primary={item} />
+            // </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        
+      </main>
+    </div>
+  );
 }
 
-export default App;
+PermanentDrawerLeft.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(PermanentDrawerLeft);
